adb9490f19e80aaa8f64e8be457536fcd85969e5
Vitor <vitordutranasc@gmail.com>
2026-04-29 22:54:34 -0300

    springtools

--- src/main/java/br/belval/api/controller/ProdutoController.java
+++ src/main/java/br/belval/api/controller/ProdutoController.java
@@ -1,29 +1,29 @@
 package br.belval.api.controller;
-
+ 
 import java.time.LocalDateTime;
-
+ 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
+ 
 import br.belval.api.model.Produto;
 import br.belval.api.repository.ProdutoRepository;
-
+ 
 @RestController
 public class ProdutoController {
-	
 	@Autowired
 	private ProdutoRepository repository;
 	
+	@GetMapping("/produtos")
 	public ResponseEntity<Iterable<Produto>> obterProdutos() {
 		return ResponseEntity
 			.status(HttpStatus.OK)
 			.body(repository.findAll());
 	}
-	
 	//curl POST http://localhost:8080/produtos -H "Content-Type: application/json; Charset=utf-8" -d @produto-pao.json
 	@PostMapping("/produtos")
 	public ResponseEntity<Produto> criarProduto(@RequestBody Produto produto) {
@@ -35,8 +35,9 @@ public class ProdutoController {
 		repository.save(produto);
 		
 		return ResponseEntity
+				
 				.status(HttpStatus.CREATED)
 				.body(produto);
 	}
-
+ 
 }
